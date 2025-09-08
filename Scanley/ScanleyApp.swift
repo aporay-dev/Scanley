6661be419eccca17ec3e5bc57ef2f7caa2723785
Anand Poray <anandporay@gmail.com>
2025-09-08 12:06:45 -0400

    Initial commit. Blank project

---
+++ Scanley/ScanleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanleyApp.swift
+//  Scanley
+//
+//  Created by Anand Poray on 2025-09-08.
+//
+
+import SwiftUI
+
+@main
+struct ScanleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
